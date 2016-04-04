a65b9680b94c64c69357773f87f00999d3e24498
Craig Lonsdale <craig@meerkats.com.au>
2016-04-04 16:53:32 +0800

    Started work to pull out user info from messages and store them locally

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from slackclient import SlackClient
 # ['<@U0XKTUM2S>: <@U0255R6S3>']
 def main():
     userRegex = re.compile('(<@[A-Z0-9]\w+>)');
-    userMap = {
-        'jimmyhillis' : '@U0255R6S3'
-    }
     try:
         config = {}
         # Read in our config stuff
         with open('config.json', 'r') as f:
             config = json.load(f)
+        userMap = {
+            config['SlackBotId'] : 'bantonio'
+        }
+        print(userMap)
         sc = SlackClient(config['SlackApiKey'])
         if sc.rtm_connect():
             while True:
@@ -23,7 +24,20 @@ def main():
                     for userRefs in messageWithUserRefs:
                         # Find if the message is referencing Bantonio
                         if config['SlackBotId'] in userRefs:
-                            sc.rtm_send_message(channel='C0Q5816U9', message='I see ya')
+                            sc.rtm_send_message(channel='C0Q5816U9', message='Let the banter begin!')
+                            # Check out our usermap to see if we recognise anyone else
+                            if userMap.keys() in userRefs:
+                                mappedUsers = [userMap.get(user) for user in userRefs if user in userMap.keys()]
+                                sc.rtm_send_message(channel='C0Q5816U9', message='I see you mentioned')
+                            else:
+                                print('Adding referenced user')
+                                newUsersId = [userRef for userRef in userRefs if userRef != config['SlackBotId']]
+                                # Find the users details
+                                print(newUsersId[0][2:-1])
+                                newUserInfo = sc.api_call('users.info', user=newUsersId[0][2:-1])
+                                print(newUserInfo)
+                                userMap.update(newUsers)
+                                print(userMap)
                 time.sleep(1)
         else:
             print("Issue connecting!!!")
